bdef673a5653dd9f39337e8d372edc2524685a57
Thainnt <thai164@gmail.com>
2021-10-05 23:40:55 +0000

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+  $('#tweet-text').keyup(function() {
+    const counterEl = $(this).parents('body').find('.counter');
+    counterEl[0].value = 140 - $(this).val().length;
+
+    if (counterEl[0].value < 0) {
+      counterEl.addClass('counter-over-limit');
+    } else {
+      counterEl.removeClass('counter-over-limit');
+    }
+
+  })
+});
